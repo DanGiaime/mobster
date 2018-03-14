30de08c7d907ee4959fc96bd10871e26d045e5c8
test <test@intuit.com>
2018-03-14 15:34:02 -0700

    Update to automatically include author emails

--- mobster-commit.sh
+++ mobster-commit.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-USER_EMAIL=` git config --get user.email`
 MOBSTERS=`cat ~/Library/Application\ Support/mobster/active-mobsters`
 # NOTE: authors must have an email to be valid, so they need to be in the form "Some name, another name, ... <email@address.com>"
-git commit -v --author="$MOBSTERS <$USER_EMAIL>"
+
+mobstersArr=(${MOBSTERS//, / })
+LEFT="<"
+RIGHT=">"
+EMAIL='@intuit.com'
+SPACE=" "
+COMMA=", "
+
+COMMIT_STR=
+
+for i in "${mobstersArr[@]}"
+do
+	COMMIT_STR=$COMMIT_STR$i$SPACE$LEFT$i$EMAIL$RIGHT$COMMA
+done
+
+git commit -v --author="$COMMIT_STR"
